2ee54e267af9dfe28dc0ba5bf2188d2e46d4713a
KabuVanth <156151763+KabuVanth@users.noreply.github.com>
2024-12-16 19:10:20 +1300

    v1.1.0
    Added batch processing capability

--- GHSysID.m
+++ GHSysID.m
@@ -11,14 +11,14 @@ function GHSysID()
     outpath = uigetdir('','Select Output Directory');
     % outpath = "..\PData\0000\output"; % for testing
 
-    %%   Device Data ----------
-    [DAA_file,DAA_path] = uigetfile('*.lvm','Select Device AB/AD LVM file');
-    % DAA_file = 'Device_Ab(0000).lvm'; % for testing
-    % DAA_path = '..\PData\0000\'; % for testing
+    %%  Device Data ----------
+    % [DAA_file,DAA_path] = uigetfile('*.lvm','Select Device AB/AD LVM file');
+    DAA_file = 'Device_Ab(0000).lvm'; % for testing
+    DAA_path = '..\PData\0000\'; % for testing
 
-    [DIE_file,DIE_path] = uigetfile('*.lvm','Select Device Int/Ext LVM file');
-    % DIE_file = 'Device_In(0000).lvm'; % for testing
-    % DIE_path = '..\PData\0000\'; % for testing
+    % [DIE_file,DIE_path] = uigetfile('*.lvm','Select Device Int/Ext LVM file');
+    DIE_file = 'Device_In(0000).lvm'; % for testing
+    DIE_path = '..\PData\0000\'; % for testing
 
     % 1st column Angle Data, 2nd column Trq Data
     [Device_AA(:,1), Device_AA(:,2)] = dataPrep(DAA_file, DAA_path);
@@ -27,28 +27,36 @@ function GHSysID()
     [Device_IE(:,1), Device_IE(:,2)] = dataPrep(DIE_file, DIE_path);
     sFRF_DIE = tfestimate(detrend(Device_IE(:,1),1),detrend(Device_IE(:,2),1),Dwin,Dov*Dwin,10001,Fs);
     
-    %%   User Data -----------
+    %%  User Data -----------
     [file,path] = uigetfile('*.lvm','Select LVM file','MultiSelect','on');
     % file = '0%_Ab(0000)L.lvm'; % for testing
     % path = '..\PData\0000\'; % for testing
-    
-    [InAngle,OutTrq,fileName,exptype] = dataPrep(file,path);
 
     %% Calculation and Output
-    switch exptype
-        case {'Ab','Ad'}
-            [Pars,mnTrq,VAF_NP,VAF_Par,sFRF_par,sFRF,coh,ftfArray] = PCalc(Fs,InAngle,OutTrq,sFRF_DAA);
-        case {'In','Ex'}
-            [Pars,mnTrq,VAF_NP,VAF_Par,sFRF_par,sFRF,coh,ftfArray] = PCalc(Fs,InAngle,OutTrq,sFRF_DIE);
+    % In case a single file is selected
+    if ischar(file)
+        file = cellstr(file);
     end
 
-    writematrix(Pars,outpath+"\"+string(fileName)+"_Par.csv");
-    writematrix(mnTrq,outpath+"\"+string(fileName)+"_meanTrq.csv");
-    writematrix(VAF_NP,outpath+"\"+string(fileName)+"_VAFNP.csv");
-    writematrix(VAF_Par,outpath+"\"+string(fileName)+"_VAFPar.csv");
-    writematrix(sFRF_par,outpath+"\"+string(fileName)+"_sFRFPar.csv");
-    writematrix(sFRF,outpath+"\"+string(fileName)+"_sFRF.csv");
-    writematrix(coh,outpath+"\"+string(fileName)+"_coh.csv");
-    writematrix(ftfArray,outpath+"\"+string(fileName)+"_ftf.csv");
+    % For loop is for multiple file selections
+    for fno = 1:length(file)
+        [InAngle,OutTrq,fileName,exptype] = dataPrep(char(file(fno)),path);
+
+        switch exptype
+            case {'Ab','Ad'}
+                [Pars,mnTrq,VAF_NP,VAF_Par,sFRF_par,sFRF,coh,ftfArray] = PCalc(Fs,InAngle,OutTrq,sFRF_DAA);
+            case {'In','Ex'}
+                [Pars,mnTrq,VAF_NP,VAF_Par,sFRF_par,sFRF,coh,ftfArray] = PCalc(Fs,InAngle,OutTrq,sFRF_DIE);
+        end
+
+        writematrix(Pars,outpath+"\"+string(fileName)+"_Par.csv");
+        writematrix(mnTrq,outpath+"\"+string(fileName)+"_meanTrq.csv");
+        writematrix(VAF_NP,outpath+"\"+string(fileName)+"_VAFNP.csv");
+        writematrix(VAF_Par,outpath+"\"+string(fileName)+"_VAFPar.csv");
+        writematrix(sFRF_par,outpath+"\"+string(fileName)+"_sFRFPar.csv");
+        writematrix(sFRF,outpath+"\"+string(fileName)+"_sFRF.csv");
+        writematrix(coh,outpath+"\"+string(fileName)+"_coh.csv");
+        writematrix(ftfArray,outpath+"\"+string(fileName)+"_ftf.csv");
+    end
 
 end
